518e5847719882610c37d1d8656b7bfe2e3e8049
bhimes3 <weakgore@gmail.com>
2025-03-25 15:06:37 -0700

    add no parem run

--- Gradle/JavaGradle/src/main/java/Multiply.java
+++ Gradle/JavaGradle/src/main/java/Multiply.java
@@ -1,29 +1,33 @@
 import java.io.*;
+
 /**
- * Purpose: demonstrate simple Java Muliply class with command line,
+ * Purpose: demonstrate simple Java Multiply class with command line,
  * jdb debugging, and Gradle build file.
  *
  * Ser321 Foundations of Distributed Applications
- * @author Aman Kaushik akaush13@asu.edu
- *         Software Engineering, CIDSE, ASU Poly
- * @version March 2020
-  * @version August 2020 Alexandra Mehlhase changs for Gradle
+ * @author Aman Kaushik
+ * @version August 2020 Alexandra Mehlhase changes for Gradle
  */
 public class Multiply {
-   public static void main (String args[]) {
-    if (args.length == 2) {
-        int argX = 0;
-        int argY = 0;
-        try {
-          argX = Integer.parseInt(args[0]);
-          argY = Integer.parseInt(args[1]);
-        } catch (Exception e) {
-          System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
-          System.exit(1);
+    public static void main(String args[]) {
+        int argX = 1;
+        int argY = 2;
+
+        if (args.length == 2) {
+            try {
+                argX = Integer.parseInt(args[0]);
+                argY = Integer.parseInt(args[1]);
+            } catch (Exception e) {
+                System.out.println("Arguments: " + args[0] + ", " + args[1] + " must be integers.");
+                System.exit(1);
+            }
+        } else if (args.length == 0) {
+            System.out.println("No arguments provided, using default values: " + argX + ", " + argY);
+        } else {
+            System.out.println("Invalid number of arguments. Please provide exactly 2 integers.");
+            System.exit(1);
         }
-        System.out.println(argX + " * " + argY + " = " + argX * argY);
-      } else {
-          System.out.println("Exactly 2 arguments should be provided.\n gradle run --args='1 2'");
-      }
+
+        System.out.println(argX + " * " + argY + " = " + (argX * argY));
     }
 }
